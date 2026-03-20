431ef30b5a5ddbb594f0571d6a6b7a0c0f61af6a
Paulo <pauloandre050619@gmail.com>
2026-03-20 09:03:39 -0300

    Ajustando o script de arquivo vinculado

--- navegacao-pastas/extracao-arquivo.sql
+++ navegacao-pastas/extracao-arquivo.sql
@@ -1,2 +1,4 @@
 -- INSERTS para tabela de arquivos
 
+INSERT INTO arquivos_digitais (nome_arquivo, path_arquivo, tipo_mime, tamanho_bytes, hash_md5, ocr_status, conteudo_ocr, chave_lote) 
+                VALUES ('teste.pdf', 'C:/Users/paulo/OneDrive/Documentos/SUAS/Projetos/Teste-script/caminho-destino/6bf4e406-a172-466d-9c35-180660b82300_teste.pdf', 'application/pdf', 1092582, '314be417b6b5759a037a0a289024f001', 'false', 'OCR pendente para processamento posterior', 'L284');
